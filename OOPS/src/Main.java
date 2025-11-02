7ad7c24a18dde2c46f7bce99ec0500b491f30098
YashBorekar <yashborekar1@gmail.com>
2025-11-02 15:16:40 +0530

    Need of Object/* Here objectcomes in */

--- OOPS/src/Main.java
+++ OOPS/src/Main.java
@@ -1,6 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    System.out.println("HELLO WORLD");
-    System.out.println("Hey");
+    // Store 5 roll nos
+    int[] Student = {12,13,4,56,76};
+
+    // Store name of 5 students
+    String[] st_name = {"Yash","Raj","suraj","Kunal","Rohit"};
+
+    // Store marks of 5 students
+    float[] marks = {20,25.50f,30,31,45};
+
+    /* What if we want to store this data at one place for each student is there any datatype which
+    can do this? {roll_no,name,marks}, no such datatype is there even arrays can store only single type
+    so we can store data of student in multiple array? but how to track or check if the specific
+    value in array belongs to that particular student? so this the problem basically
+    */
 }
